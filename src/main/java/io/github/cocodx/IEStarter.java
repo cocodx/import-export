d279f31fac995e38cfa95edd20fe767fdceac386
刘刚 <2cevening@gmail.com>
2022-08-30 04:00:52 +0800

    feature(*):导入依赖jar包，测试项目，正常启动

---
+++ src/main/java/io/github/cocodx/IEStarter.java
@@ -0,0 +1,20 @@
+package io.github.cocodx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * 导入-导出启动类
+ * @author amazfit
+ * @date 2022-08-30 上午3:58
+ **/
+@SpringBootApplication
+public class IEStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IEStarter.class,args);
+    }
+
+
+}
